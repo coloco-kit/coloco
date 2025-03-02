9d38ea93006980cbe159a565e13dd7adac489a98
Channel Cat <channelcat@gmail.com>
2025-03-02 02:03:47 -0800

    WIP

--- besto/index.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  import { testQq } from "./api";
-
-  const results = testQq({ body: { name: "John" } });
-</script>
-
-I AM THE BESTO {$results.loading} - {$results.data?.message} - {$results.error}
